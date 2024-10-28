c41c8894bb7b91443e6b4d3d75a5fb5f1708191c
Raghu K <raghuk.vit@gmail.com>
2024-10-28 06:45:13 +0530

    RoboShop Kubernetes

--- run.sh
+++ run.sh
@@ -19,6 +19,6 @@ if [ -z "${NEW_RELIC_APP_NAME}" ]; then
 fi
 
 #sed -i -e "s/LICENSE_KEY/${NEW_RELIC_LICENSE_KEY}/" /app/newrelic/newrelic.yml
-export PATH=$PATH:/opt/amazon-corretto-17.0.13.11.1-linux-x64/bin
-/opt/amazon-corretto-17.0.13.11.1-linux-x64/bin/java -javaagent:/app/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENSE_KEY} -jar /app/target/shipping-1.0.jar
+export PATH=$PATH:/opt/amazon-corretto-17.0.12.7.1-linux-x64/bin
+/opt/amazon-corretto-17.0.12.7.1-linux-x64/bin/java -javaagent:/app/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENSE_KEY} -jar /app/target/shipping-1.0.jar
 
